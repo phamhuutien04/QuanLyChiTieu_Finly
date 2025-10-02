9b76eb9526737ca6a01b017951bc6afec7baed1d
phamhuutien04 <tienreport808@gmail.com>
2025-10-02 23:32:50 +0700

    v1
    1

--- app/src/main/java/com/example/quanlychitieu_finly/CategoryFragment.kt
+++ app/src/main/java/com/example/quanlychitieu_finly/CategoryFragment.kt
@@ -9,6 +9,7 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
 class CategoryFragment : Fragment() {
 
@@ -19,6 +20,7 @@ class CategoryFragment : Fragment() {
     private lateinit var tvTotal: TextView
     private lateinit var underlineSpending: View
     private lateinit var underlineIncome: View
+    private lateinit var fabAddCategory: ExtendedFloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,14 +28,16 @@ class CategoryFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_category, container, false)
 
+        // Ánh xạ view
         recyclerView = view.findViewById(R.id.rcvCategories)
         tabSpending = view.findViewById(R.id.tabSpending)
         tabIncome = view.findViewById(R.id.tabIncome)
         underlineSpending = view.findViewById(R.id.underlineSpending)
         underlineIncome = view.findViewById(R.id.underlineIncome)
         tvTotal = view.findViewById(R.id.tvTotal)
+        fabAddCategory = view.findViewById(R.id.fabAddCategory)
 
-        // Chi tiêu
+        // Danh mục Chi tiêu
         val spendingCategories = listOf(
             Category(R.drawable.ic_food, "Ăn uống", "2,000,000đ"),
             Category(R.drawable.ic_car, "Di chuyển", "800,000đ"),
@@ -42,14 +46,15 @@ class CategoryFragment : Fragment() {
             Category(R.drawable.ic_sk, "Y tế", "2,000,000đ"),
             Category(R.drawable.ic_cinema, "Giải trí", "800,000đ"),
             Category(R.drawable.ic_sports, "Thể thao", "1,500,000đ"),
-            Category(R.drawable.ic_adds, "Khác", "")
+            Category(R.drawable.ic_adds, "Khác", "2,000,000đ")
         )
 
-        // Thu nhập
+        // Danh mục Thu nhập
         val incomeCategories = listOf(
             Category(R.drawable.ic_wage, "Lương", "10,000,000đ"),
             Category(R.drawable.ic_wages, "Thưởng", "2,000,000đ"),
-            Category(R.drawable.ic_adds, "Quà tặng", "")
+            Category(R.drawable.ic_adds, "Quà tặng", "2,000,000đ"),
+            Category(R.drawable.ic_adds, "Khác", "2,000,000đ")
         )
 
         // Mặc định hiển thị Chi tiêu
@@ -58,6 +63,7 @@ class CategoryFragment : Fragment() {
         highlightTab(tabSpending)
         adapter.setTab(true)
 
+        // Chuyển tab → Chi tiêu
         tabSpending.setOnClickListener {
             showCategoryList(spendingCategories)
             updateTotal(spendingCategories)
@@ -65,6 +71,7 @@ class CategoryFragment : Fragment() {
             adapter.setTab(true) // Chi tiêu → đỏ
         }
 
+        // Chuyển tab → Thu nhập
         tabIncome.setOnClickListener {
             showCategoryList(incomeCategories)
             updateTotal(incomeCategories)
@@ -72,9 +79,19 @@ class CategoryFragment : Fragment() {
             adapter.setTab(false) // Thu nhập → xanh
         }
 
+//         Ấn nút Thêm → sang fragment khác
+        fabAddCategory.setOnClickListener {
+            val newFragment = AddCategoryFragment()
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, newFragment) // FrameLayout chứa fragment
+                .addToBackStack(null) // để nhấn Back quay lại
+                .commit()
+        }
+
         return view
     }
 
+    // Hiển thị danh sách Category
     private fun showCategoryList(list: List<Category>) {
         adapter = CategoryAdapter(list) { category ->
             // TODO: xử lý khi click item nếu cần
@@ -83,6 +100,7 @@ class CategoryFragment : Fragment() {
         recyclerView.adapter = adapter
     }
 
+    // Tính tổng số tiền
     private fun updateTotal(list: List<Category>) {
         var total = 0L
         for (c in list) {
@@ -95,6 +113,7 @@ class CategoryFragment : Fragment() {
         tvTotal.text = "%,dđ".format(total)
     }
 
+    // Đổi màu tab + underline
     private fun highlightTab(selected: TextView) {
         if (selected == tabSpending) {
             tabSpending.setTextColor(ContextCompat.getColor(requireContext(), R.color.bluesky))
